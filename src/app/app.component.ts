3695a5c28ab94c15c977f93306f03817b9c7963d
vikasbhamkar <121518615+vikasbhamkar@users.noreply.github.com>
2023-02-23 23:02:54 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'protest';
+  allData: any;
+  data: any;
+  total: any;
+  recordone = false;
+  norecord = true;
+  onerecord = false;
+  currentPage = 1;
+  recordsPerPage = 3;
+  constructor(private http: HttpClient) {
+    this.getData().subscribe((data) => {
+      this.total = data.length;
+      this.data = data.slice(0, this.recordsPerPage);
+    })
+  }
+  getData() {
+    return this.http.get<any>('assets/data.json').pipe(
+      tap(data => this.allData = data)
+    );
+  }
+  getnextpage() {
+    this.currentPage++;
+    const startIndex = (this.currentPage - 1) * this.recordsPerPage;
+    const endIndex = startIndex + this.recordsPerPage;
+    this.data = this.allData.slice(startIndex, endIndex);
+    if (startIndex == this.total - 1) {
+      this.onerecord = true;
+    }
+    if (startIndex > this.total) {
+      this.norecord = false;
+      this.recordone = true;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule} from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {NgxPaginationModule} from 'ngx-pagination';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NgxPaginationModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
